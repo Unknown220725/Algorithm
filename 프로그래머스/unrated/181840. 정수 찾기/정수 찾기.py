fca003052680b15c6d0290162dfb249ad505599d
Unknown220725 <109930570+Unknown220725@users.noreply.github.com>
2023-12-06 18:43:06 +0900

    [level unrated] Title: 정수 찾기, Time: 0.00 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.py
@@ -0,0 +1,12 @@
+def solution(num_list, n):
+    answer = 0
+    
+    
+#     if(n in num_list):
+#         answer = n
+        
+#     else:
+#         answer = 0
+    
+    
+    return 1 if(n in num_list) else 0
