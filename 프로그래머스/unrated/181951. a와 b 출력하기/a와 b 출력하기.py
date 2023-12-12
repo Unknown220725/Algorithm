315d4c3e6c6dc591a12fab35aedaeef2886ea02d
Unknown220725 <109930570+Unknown220725@users.noreply.github.com>
2023-12-12 11:02:13 +0900

    [level unrated] Title: a와 b 출력하기, Time: 11.78 ms, Memory: 7.64 MB -BaekjoonHub

--- 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.py
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.py
@@ -2,5 +2,7 @@ a, b = map(int, input().strip().split(' '))
 # print(f"a ={a}")
 # print(f"b ={b}")
 
-print("a =",a)
-print("b =",b)
+# print("a =",a)
+# print("b =",b)
+
+print(f'a = {a}\nb = {b}')
