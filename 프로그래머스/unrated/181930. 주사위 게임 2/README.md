a0e957e79af9ff79e3742cfb70d79f6ebecbbbda
Unknown220725 <109930570+Unknown220725@users.noreply.github.com>
2023-12-12 13:16:59 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/README.md
+++ 프로그래머스/unrated/181930. 주사위 게임 2/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 주사위 게임 2 - 181930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.01 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 13:11:1
+2023년 12월 2일 13:17:0
 
 ### 문제 설명
 
--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -4,13 +4,14 @@ def solution(a, b, c):
     print(a!=b and a!=c and b!=c)
     print(a!=b!=c)
     
-    #if (a!=b and a!=c and b!=c):    
-    #if (a!=b)!=c:
+    #if (a!=b and a!=c and b!=c):
     if (a!=b!=c!=a):
         print(a,b,c)
         answer = a+b+c
     
-    elif (a==b and a==c and b==c):
+    #elif (a==b and a==c and b==c):
+    elif (a==b==c):
+    
         print(a,b,c)
         answer = (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
         
