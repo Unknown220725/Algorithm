d8a762ff16f820f9328ff63c3a608c2375e546e5
Unknown220725 <109930570+Unknown220725@users.noreply.github.com>
2023-12-13 11:25:21 +0900

    [level unrated] Title: 배열의 원소만큼 추가하기, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    
+    for i in arr: #arr 순회
+        #print(type(i))
+        for x in range(i): #i반복
+            #print(i)
+            answer.append(i)    
+    
+    return answer
