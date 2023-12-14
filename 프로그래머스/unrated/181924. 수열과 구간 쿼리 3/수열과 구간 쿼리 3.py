3c9782695aee2c175cbeadf022a8b695ca2fb5b4
Unknown220725 <109930570+Unknown220725@users.noreply.github.com>
2023-12-14 15:11:20 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -2,11 +2,17 @@ def solution(arr, queries):
     answer = []
     temp = 0
     
-    for i in range(len(queries)):
-        x,y = queries[i]
-        temp = arr[x]
-        arr[x] = arr[y]
-        arr[y] = temp
-  
+    # for i in range(len(queries)):
+    #     x,y = queries[i]
+    #     temp = arr[x]
+    #     arr[x] = arr[y]
+    #     arr[y] = temp
+    
+    for a,b in queries:
+        #   0      3      3      0
+        arr[a],arr[b]=arr[b],arr[a]
+
     answer = arr
     return answer
+
+  
