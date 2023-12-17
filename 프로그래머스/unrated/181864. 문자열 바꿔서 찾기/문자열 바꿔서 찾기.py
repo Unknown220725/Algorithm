470d8aed3196c0b8745778c594970c5e6b6df2da
Unknown220725 <109930570+Unknown220725@users.noreply.github.com>
2023-12-17 14:38:17 +0900

    [level unrated] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,17 +1,17 @@
 def solution(myString, pat):
-    myString = list(myString)
+#     myString = list(myString)
     
-    for i in range(len(myString)):
-        if (myString[i] == 'A'):
-             myString[i] = 'B'
-        else:
-             myString[i] = 'A'   
+#     for i in range(len(myString)):
+#         if (myString[i] == 'A'):
+#              myString[i] = 'B'
+#         else:
+#              myString[i] = 'A'   
                 
-    myString = ''.join(myString)
+#     myString = ''.join(myString)
     #print(myString)
     
     # String 타입 문자열 수정을 위해 리스트처럼 인덱스로 접근은 할 수 있지만 수정은 불가능한 것이다
-    #myString = myString.replace('B','C').replace('A','B').replace('C','A')    
+    myString = myString.replace('B','C').replace('A','B').replace('C','A')    
     # list() 함수 이용
     #myString.split() # split() 함수 이용
     #print(list(myString))
