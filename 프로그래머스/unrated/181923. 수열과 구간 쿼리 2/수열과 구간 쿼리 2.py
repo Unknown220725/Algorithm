160456dfdd44b4a42637621f1130eeacadb1e410
Unknown220725 <109930570+Unknown220725@users.noreply.github.com>
2023-12-17 16:36:46 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 9.95 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -0,0 +1,29 @@
+def solution(arr, queries):
+    answer = []
+
+    #쿼리즈 길이만큼 반복 3회
+    for j in range(len(queries)):
+        query = []
+        
+        #arr 슬라이싱 queries[0]~[1]까지 0,4+1
+        for i in range(queries[j][0],queries[j][1]+1):
+            if arr[i] > queries[j][2]:
+                #print(arr[i])
+                query.append(arr[i])
+            
+            #print(queries[j][0],queries[j][1])
+
+            #box.append(arr[i])
+           # print(box)
+            #2보다 크면 box에 추가
+            # if arr[i]>queries[j][2]:
+            #     query.append(arr[i])
+            #     print(query)
+            #    # print("제거")
+                
+        if(len(query)==0):
+            answer.append(-1)
+        else:
+            answer.append(min(query))
+    
+    return answer
