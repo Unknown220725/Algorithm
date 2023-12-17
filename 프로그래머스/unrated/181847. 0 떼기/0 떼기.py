61c379dd1872bc6cecb6000287181cec67727687
Unknown220725 <109930570+Unknown220725@users.noreply.github.com>
2023-12-17 13:26:16 +0900

    [level unrated] Title: 0 떼기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.py
@@ -0,0 +1,10 @@
+def solution(n_str):
+    answer = ''
+    answer = n_str.lstrip('0')
+    
+    # if (n_str[:1] == '0' and n_str[-1:] == '0') :
+    #     answer = n_str.lstrip('0')
+    # else:
+    #     answer = n_str
+    
+    return answer
