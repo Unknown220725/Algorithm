e59457493c8af307159dffb58bbcae5c183a2d0a
Unknown220725 <109930570+Unknown220725@users.noreply.github.com>
2024-03-28 23:25:34 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/README.md
+++ 프로그래머스/0/181904. 세로 읽기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 세로 읽기 - 181904 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181904) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181904?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 28일 23:22:31
+2024년 03월 28일 23:25:35
 
 ### 문제 설명
 
--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,11 +1,12 @@
 def solution(my_string, m, c):
     answer = ''
     
-    if m == 1:
-        answer = my_string
+#     if m == 1:
+#         answer = my_string
     
-    else:
-        for x in range(c-1,len(my_string),m):
-            answer += my_string[x]
+#     else:
+#         for x in range(c-1,len(my_string),m):
+#             answer += my_string[x]
             
-    return answer
+    return my_string[c-1::m]
+    # return answer
