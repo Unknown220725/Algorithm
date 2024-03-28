e9e2b6cd2817a1eef56e667c69083f28d7c603d7
Unknown220725 <109930570+Unknown220725@users.noreply.github.com>
2024-03-28 23:22:29 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -0,0 +1,11 @@
+def solution(my_string, m, c):
+    answer = ''
+    
+    if m == 1:
+        answer = my_string
+    
+    else:
+        for x in range(c-1,len(my_string),m):
+            answer += my_string[x]
+            
+    return answer
