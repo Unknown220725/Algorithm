1cb974166b7659606d7f2bfa5aebe88a794dd745
Unknown220725 <hikari1021@naver.com>
2024-10-29 22:36:52 +0900

    [level 0] Title: 암호 해독, Time: 0.18 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -0,0 +1,12 @@
+def solution(cipher, code):
+    answer = ''
+    
+    for x in range(code-1,len(cipher),code):
+        answer += cipher[x]
+        # print(answer)
+        
+    
+    
+    
+    
+    return answer
